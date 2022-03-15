22417e43feeab30b89447e33e25f8edcaa5296b9
Aikerim <akeneshbekovna92@mail.ru>
2022-03-15 20:10:56 +0500

    created a To meet model in homework and created data inside

--- homework/models.py
+++ homework/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class ToMeet(models.Model):
+    persone = models.CharField(max_length=40)
+    phone_namber =models.CharField(max_length=16)
+    date_of_meeting = models.DateField(auto_now_add=False)
+    comment = models.CharField(max_length=200)
+    is_closed = models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False)
+
+
+class Goal_for_month(models.Model):
+    goal= models.TextField
+    month = models.DateField(auto_now_add=False)
+    difficulty = models.BooleanField(default=False)
+    reason_for_goal= models.CharField(max_length=100)
+
+    
