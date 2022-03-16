b7a989f7d2450bb6458a47084491eb1f90f2d62f
Aikerim <akeneshbekovna92@mail.ru>
2022-03-16 14:15:31 +0500

    addad model

--- homework/admin.py
+++ homework/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import ToMeet, Goal_for_month
 
-
-
 admin.site.register(ToMeet)
+
 admin.site.register(Goal_for_month)
--- homework/models.py
+++ homework/models.py
@@ -5,15 +5,16 @@ class ToMeet(models.Model):
     persone = models.CharField(max_length=40)
     phone_namber =models.CharField(max_length=16)
     date_of_meeting = models.DateField(auto_now_add=False)
-    comment = models.CharField(max_length=200)
+    comment = models.TextField()
     is_closed = models.BooleanField(default=False)
     is_favorite = models.BooleanField(default=False)
 
 
 class Goal_for_month(models.Model):
-    goal= models.CharField(max_length=40)
+    goal = models.CharField(max_length=40)
     month = models.DateField(auto_now_add=False)
     difficulty = models.BooleanField(default=False)
     reason_for_goal= models.CharField(max_length=100)
+    
 
     
