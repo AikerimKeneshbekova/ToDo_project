466e637c3c339f459bbab051d49c3d851c9f055e
Aikerim <akeneshbekovna92@mail.ru>
2022-03-20 03:10:56 +0500

    created a new ToDo model and linked test.html

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Habits
+from .models import Habits, ToDo
 
 admin.site.register(Habits)
+
+admin.site.register(ToDo)
--- main/models.py
+++ main/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+class ToDo(models.Model):
+    text = models.CharField(max_length=40)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_closed =models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False)
+
 class Habits(models.Model):
     name = models.CharField(max_length=40)
     done_for_today = models.BooleanField(default=False)
